dff9968e2bdb236b12763bceb03a1d2891ec407e
Mateo Quintero <matteoquintero@gmail.com>
2023-05-12 19:54:46 -0500

    100% web page whit expiration data

--- js/expiration.js
+++ js/expiration.js
@@ -1,16 +1,13 @@
-
-
 const storedExpiration = localStorage.getItem('expiration');
 if (storedExpiration) {
-    const expiration = new Date(parseInt(storedExpiration));
-    if (expiration > new Date()) {
+    const nowValid = new Date();
+    const expirationValid = new Date(parseInt(storedExpiration));
+    if (nowValid >= expirationValid) {
+        console.log('oli')
         localStorage.clear(); 
-    } else {
-        const now = new Date();
-        const expiration = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000); // caduca en una semana (7 días x 24 horas x 60 minutos x 60 segundos x 1000 milisegundos)
-        localStorage.setItem('expiration', expiration.getTime().toString());
-    }
+    } 
 }else{
+    console.log('si oli')
     const now = new Date();
     const expiration = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000); // caduca en una semana (7 días x 24 horas x 60 minutos x 60 segundos x 1000 milisegundos)
     localStorage.setItem('expiration', expiration.getTime().toString());
